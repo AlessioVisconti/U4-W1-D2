cc4b92358ec07cfd04c80c64fa46c6da4289f39b
AlessioVisconti <alessiovisconti00@gmail.com>
2025-11-04 15:26:52 +0100

    Aggiungere i file di progetto.

---
+++ U4-W1-D2/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D2
+{
+    internal class Persona
+    {
+        public Persona(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public void GetName()
+        {
+            Console.WriteLine($"The name is: {Name}");
+        }
+        public void GetSurname()
+        {
+            Console.WriteLine($"The surname is: {Surname}");
+        }
+        public void GetAge()
+        {
+            Console.WriteLine($"The age is: {Age}");
+        }
+    }
+}
---
+++ U4-W1-D2/Program.cs
@@ -0,0 +1,7 @@
+﻿using U4_W1_D2;
+
+var Francesco = new Persona("Francesco", "Schettino", 64);
+
+Francesco.GetName();
+Francesco.GetAge();
+Francesco.GetSurname();
