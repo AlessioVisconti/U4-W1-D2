22c90022482f28106c0c54fdcce927c31e036e05
AlessioVisconti <alessiovisconti00@gmail.com>
2025-11-04 15:34:33 +0100

    add comments

--- U4-W1-D2/Persona.cs
+++ U4-W1-D2/Persona.cs
@@ -8,15 +8,17 @@ namespace U4_W1_D2
 {
     internal class Persona
     {
+        //costruttore ovvero lo stampino
         public Persona(string name, string surname, int age)
         {
             Name = name;
             Surname = surname;
             Age = age;
         }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int Age { get; set; }
+        //setting
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public int? Age { get; set; }
         public void GetName()
         {
             Console.WriteLine($"The name is: {Name}");
