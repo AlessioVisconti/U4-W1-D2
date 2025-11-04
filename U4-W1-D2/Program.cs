c61af3204cf90c40481e06a98768f1bb1d2e5db0
AlessioVisconti <alessiovisconti00@gmail.com>
2025-11-04 15:50:19 +0100

    end of ex

--- U4-W1-D2/Program.cs
+++ U4-W1-D2/Program.cs
@@ -5,3 +5,7 @@ var Francesco = new Persona("Francesco", "Schettino", 64);
 Francesco.GetName();
 Francesco.GetAge();
 Francesco.GetSurname();
+Console.WriteLine(Francesco.GetNameReturn());
+Console.WriteLine(Francesco.GetSurnameReturn());
+Console.WriteLine(Francesco.GetAgeReturn());
+Francesco.GetDeatails();
